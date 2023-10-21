ced0467e959fae74fa23b15ac12f283a5398471d
Kaynan Wallen <kaynanwallen@gmail.com>
2023-10-21 16:52:49 -0300

    Initial commit

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,16 @@
+@extends('login.layout')
+@section('title', 'Pagina Home')
+@section('content')
+
+<div class="container py-4">
+    <div class="row">
+        <div class="col sm-12">
+            <p class="alert alert-info">
+                Lorem ipsum dolor sit amet.
+            </p>
+        </div>
+    </div>
+</div>
+
+
+@endsection
